d84a6fa89519232be34fdfc1cb5334797d0fc9a7
Virman44 <56293360+Virman44@users.noreply.github.com>
2021-10-09 15:13:13 +0300

    Update Click.c

--- Click.c
+++ Click.c
@@ -1,23 +1,40 @@
 #define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h> 
+#include <stdlib.h>
 #include <math.h>
 #include <iostream>
-#include<cfloat>
+#include <cfloat>
 
 int main(void) {
 	char pass;
-	int j = 0;
-	double max = DBL_MIN;
-	double compare = 0.0;
-	char openfile[] = "n12_0.01_des.plt";
-	FILE* data = NULL;
+	char openfile[10][10] = { "file1", "file2", "file3", "file4", "file5", "file6", "file7", "file8", "file9", "file10"};
 
-	data = fopen(openfile, "r");
-    if (data == NULL) {
-        printf("Error opening file");
-        exit(0);
-    }
-	
+	int system(const char* command);
+
+	system("scp username@ip:/path/to/file1 .");
+	system("scp username@ip:/path/to/file2 .");
+	system("scp username@ip:/path/to/file3 .");
+	system("scp username@ip:/path/to/file4 .");
+	system("scp username@ip:/path/to/file5 .");
+	system("scp username@ip:/path/to/file6 .");
+	system("scp username@ip:/path/to/file7 .");
+	system("scp username@ip:/path/to/file8 .");
+	system("scp username@ip:/path/to/file9 .");
+	system("scp username@ip:/path/to/file10 .");
+
+	for (int a = 0; a < 10; a++) {
+		int j = 0;
+		double max = DBL_MIN;
+		double compare = 0.0;
+
+		FILE* data = NULL;
+
+		data = fopen(openfile[a], "r");
+		if (data == NULL) {
+			printf("Error opening file");
+			exit(0);
+		}
+
 		while (j != 1) {
 			do {
 				if (((pass = fgetc(data)) != '{') && (j == 0)) {
@@ -44,6 +61,7 @@ int main(void) {
 			}
 		}
 
-	printf("%1.14e", max);
-	fclose(data);
+		printf("Total current for %s = %1.14e", openfile[a], max);
+		fclose(data);
+	}
 }
